4f4dc832f2e1033b76d83fe29fe85dcad38d6fa6
Andrew Zhu <me@andrew-zhu.com>
2022-05-04 16:24:19 -0700

    fix: attributeerror if null passed to optional identifier field

--- automation_common/validation/field_validators.py
+++ automation_common/validation/field_validators.py
@@ -1,4 +1,14 @@
-def str_is_identifier(value: str):
+from typing import Optional
+
+import pydantic.fields
+
+
+def str_is_identifier(value: Optional[str], field: pydantic.fields.ModelField):
+    """If a str is given, it must be an identifier."""
+    if value is None:
+        if field.allow_none:
+            return value
+        raise ValueError("None is not an allowed value")
     if not value.isidentifier():
         raise ValueError("value must be a valid identifier")
     return value
