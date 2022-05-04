6c3ad22a8291983b67dd2261a6f13f19dee1298e
Andrew Zhu <me@andrew-zhu.com>
2022-05-04 13:44:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# automation-common
+
+This library contains common utilities for Automation across Avrae services.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh.readlines()]
+
+setuptools.setup(
+    name="avrae-automation-common",
+    version="3.6.0",
+    author="Andrew Zhu",
+    author_email="andrew@zhu.codes",
+    description="Common automation utilities for Avrae",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avrae/automation-common",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+)
